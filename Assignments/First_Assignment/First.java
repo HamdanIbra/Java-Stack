76f62f0d4a8e43d3cd4f56ad8c0e9709d7b669c4
Ibrahim Hamdan <ibra111296@gmail.com>
2023-04-19 01:12:02 +0300

    initial commit + added my first assignment

---
+++ Assignments/First_Assignment/First.java
@@ -0,0 +1,7 @@
+public class First {
+	public static void main(String[] args) {
+		System.out.println("My name is Ibrahim Hamdan");
+        System.out.println("I am 26 years old");
+        System.out.println("My home town is Ya'bad, Jenin");
+	}
+}
